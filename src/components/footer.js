4f526843b33f0e6703722c65e661f550af11be31
Diego Rubilar <diegorubilartagle@gmail.com>
2020-03-10 20:38:21 -0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,29 @@
 
 import React from "react"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import { globalHistory as history } from '@reach/router'
+
 
 const Footer = () => {
-
+    
+    const { location, navigate } = history
+    console.log( location.pathname ) 
     return (
      <Container className="sticky-bottom">
          
         <br></br>
-         <p className={
-              window.location.pathname === "/" ? "white" : "grey"
+         <p className={ 
+             location.pathname === "/" ? "white" : "grey" 
             }>
          Made with Gatsby / React and Bootstrap with ❤︎ by <a href="https://twitter.com/rugleh">@Rugleh</a>
          </p>
            
          
-     </Container>
+
+     </Container>   
     )
+
+    
   }
 
 
